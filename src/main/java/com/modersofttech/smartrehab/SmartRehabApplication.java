4a6c4126388d3532c27aba4a03a9ac09d78749ee
Berm-Macbook <bsongkran@hotmail.com>
2018-05-26 18:02:58 +0700

    First Commit

---
+++ src/main/java/com/modersofttech/smartrehab/SmartRehabApplication.java
@@ -0,0 +1,12 @@
+package com.modersofttech.smartrehab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRehabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartRehabApplication.class, args);
+    }
+}
---
+++ src/test/java/com/modersofttech/smartrehab/SmartrehabApplicationTests.java
@@ -0,0 +1,16 @@
+package com.modersofttech.smartrehab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartrehabApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
